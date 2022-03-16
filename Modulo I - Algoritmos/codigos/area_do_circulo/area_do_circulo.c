359920d2c4c842b9d878f7dab3536ff868c1bca1
rubskaiserman <rubskaiserman@gmail.com>
2022-03-16 18:28:45 -0300

    Mudanças

---
+++ Modulo I - Algoritmos/codigos/area_do_circulo/area_do_circulo.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+void main(void){
+    double pi = 3.14159;
+    double r;
+    scanf("%le", &r);
+    double a = pi * r * r;
+    printf("A área para o raio inserido é: %f\n", a);
+}
+
