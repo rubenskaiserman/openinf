b70bf86fe396b4243eae59c53c0c109daf983a94
rubskaiserman <rubskaiserman@gmail.com>
2022-04-02 14:25:30 -0300

    Equation

---
+++ 1_algoritmos/gabarito_exercicios/1_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Olá, mundo!\n");
+    
+    return 0;
+}
---
+++ 1_algoritmos/gabarito_exercicios/2_dobro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+    printf("Insira um valor inteiro: ");
+    scanf("%d", &num);
+    printf("O dobro do valor é: %d\n", num * 2);
+
+
+    return 0;
+}
